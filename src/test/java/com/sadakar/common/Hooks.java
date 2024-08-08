e9ee5ff96fb5adac44c739119e79941c3568e2b1
mayssa <mayssalarguech@ieee.org>
2024-08-08 16:54:17 +0100

    fixed

--- src/test/java/com/sadakar/common/Hooks.java
+++ src/test/java/com/sadakar/common/Hooks.java
@@ -1,26 +1,32 @@
 package com.sadakar.common;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;  // Assurez-vous d'avoir cette dépendance dans votre projet
 import java.time.Duration;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks extends BasePage {
 
-	@Before //Cucumber Before Hook
-	public static void setupDriver() throws InterruptedException {
-		System.setProperty("webdriver.gecko.driver", "C:\\Projects\\CMA\\projects\\geckodriver-v0.34.0-win64\\geckodriver.exe");
-		driver = new FirefoxDriver();
-		//driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    @Before // Cucumber Before Hook
+    public static void setupDriver() throws InterruptedException {
+      
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
 
-	}
-
-	@After // Cucumber After hook
-	public static void quitDriver() throws Exception {
-		driver.quit();
-	}
-
+    @After // Cucumber After hook
+    public static void quitDriver() throws Exception {
+      
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
+
+
