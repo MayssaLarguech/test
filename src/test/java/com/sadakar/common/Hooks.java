d8b7ecf137bfa4dffc35a6d2ee287a7a36e68643
mayssa <mayssalarguech@ieee.org>
2024-08-09 14:54:44 +0100

    last commit

--- src/test/java/com/sadakar/common/Hooks.java
+++ src/test/java/com/sadakar/common/Hooks.java
@@ -1,7 +1,7 @@
 package com.sadakar.common;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;  // Assurez-vous d'avoir cette dépendance dans votre projet
 import java.time.Duration;
 
@@ -12,21 +12,20 @@ public class Hooks extends BasePage {
 
     @Before // Cucumber Before Hook
     public static void setupDriver() throws InterruptedException {
-      
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        // Configuration du WebDriver pour Firefox
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
         
+        // Accès à la page de connexion d'OrangeHRM
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
 
     @After // Cucumber After hook
     public static void quitDriver() throws Exception {
-      
+        // Fermeture du navigateur
         if (driver != null) {
             driver.quit();
         }
     }
 }
-
-
